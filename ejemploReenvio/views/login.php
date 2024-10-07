94fb0a8bb47ab6b92dadeb63adf90f5636212fb0
Secoroy <secoroy1@gmail.com>
2024-10-07 20:27:34 +0200

    proyecto reenvio

---
+++ ejemploReenvio/views/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <h1>Formulario de acceso</h1>
+
+    <form method="post" action="?method=auth">
+
+        <label for="name">Nombre: </label>
+        <input type="text" name="name" placeholder="Escribe..."><br><br>
+
+        <label for="password">Contraseña: </label>
+        <input type="number" name="password" placeholder="Escribe..."><br><br>
+
+        <input type="submit" value="Enviar consulta">
+
+    </form>
+    
+</body>
+</html>
